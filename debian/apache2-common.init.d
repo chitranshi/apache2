1a7f763c3c14d2bd263d9a85f5d82fbcbd4e7be1
Thom May <thom@debian.org>
2006-01-26 14:09:59 +0000

    rename from httpd2.1 to apache2
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-apache/trunk/apache2@21 01b336ce-410b-0410-9a02-a0e7f243c266

--- debian/apache2-common.init.d
+++ debian/apache2-common.init.d
@@ -1,19 +1,19 @@
 #!/bin/sh -e
 #
-# httpd2.1		This init.d script is used to start httpd2.1.
-#			It basically just calls httpd2.1ctl.
+# apache2		This init.d script is used to start apache2.
+#			It basically just calls apache2ctl.
 
 ENV="env -i LANG=C PATH=/usr/local/bin:/usr/bin:/bin"
 
-#[ `ls -1 /etc/httpd2.1/sites-enabled/ | wc -l | sed -e 's/ *//;'` -eq 0 ] && \
-#echo "You haven't enabled any sites yet, so I'm not starting httpd2.1." && \
+#[ `ls -1 /etc/apache2/sites-enabled/ | wc -l | sed -e 's/ *//;'` -eq 0 ] && \
+#echo "You haven't enabled any sites yet, so I'm not starting apache2." && \
 #echo "To add and enable a host, use addhost and enhost." && exit 0
 
-#edit /etc/default/httpd2.1 to change this.
+#edit /etc/default/apache2 to change this.
 NO_START=0
 
 set -e
-if [ -x /usr/sbin/httpd2.1 ] ; then
+if [ -x /usr/sbin/apache2 ] ; then
 	HAVE_APACHE2=1
 else
 	exit 0
@@ -22,17 +22,17 @@ fi
 . /lib/lsb/init-functions
 
 test -f /etc/default/rcS && . /etc/default/rcS
-test -f /etc/default/httpd2.1 && . /etc/default/httpd2.1
+test -f /etc/default/apache2 && . /etc/default/apache2
 if [ "$NO_START" != "0" -a "$1" != "stop" ]; then 
-        [ "$VERBOSE" != no ] && log_warning_msg "Not starting httpd2.1 - edit /etc/default/httpd2.1 and change NO_START to be 0.";
+        [ "$VERBOSE" != no ] && log_warning_msg "Not starting apache2 - edit /etc/default/apache2 and change NO_START to be 0.";
         exit 0;
 fi
 
-APACHE2="$ENV /usr/sbin/httpd2.1"
-APACHE2CTL="$ENV /usr/sbin/httpd2.1-ctl"
+APACHE2="$ENV /usr/sbin/apache2"
+APACHE2CTL="$ENV /usr/sbin/apache2-ctl"
 
 apache_stop() {
-	if `httpd2.1 -t > /dev/null 2>&1`; then
+	if `apache2 -t > /dev/null 2>&1`; then
 		# if the config is ok than we just stop normaly
 		$APACHE2 -k stop
 	else
@@ -41,14 +41,14 @@ apache_stop() {
 
 		# if pidof is null for some reasons the script exits automagically
 		# classified as good/unknown feature
-		PIDS=`pidof httpd2.1` || true
+		PIDS=`pidof apache2` || true
 
 		PID=""
 
 		# let's try to find the pid file
 		# apache2 allows more than PidFile entry in the config but only
 		# the last found in the config is used
-		for PFILE in `grep ^PidFile /etc/httpd2.1/* -r | awk '{print $2}'`; do
+		for PFILE in `grep ^PidFile /etc/apache2/* -r | awk '{print $2}'`; do
 			if [ -e $PFILE ]; then
 				PID=`cat $PFILE`
 			fi
@@ -67,7 +67,7 @@ apache_stop() {
 
 		if [ $REALPID = 1 ]; then
 			# in this case it is everything nice and dandy
-			# and we kill httpd2.1
+			# and we kill apache2
 			kill $PID
 		else
 			# this is the worst situation... just kill all of them
@@ -77,8 +77,8 @@ apache_stop() {
 			# Except, we can't do that, because it's very, very bad
 			if [ "$VERBOSE" != no ]; then
                                 echo " ... failed!"
-			        echo "You may still have some httpd2.1 processes running.  There are"
- 			        echo "processes named 'httpd2.1' which do not match your pid file,"
+			        echo "You may still have some apache2 processes running.  There are"
+ 			        echo "processes named 'apache2' which do not match your pid file,"
 			        echo "and in the name of safety, we've left them alone.  Please review"
 			        echo "the situation by hand."
                         fi
@@ -90,10 +90,10 @@ apache_stop() {
 # Stupid hack to keep lintian happy. (Warrk! Stupidhack!).
 case $1 in
 	start)
-		[ -f /etc/httpd2.1/httpd.conf ] || touch /etc/httpd2.1/httpd.conf
+		[ -f /etc/apache2/httpd.conf ] || touch /etc/apache2/httpd.conf
 		#ssl_scache shouldn't be here if we're just starting up.
-		[ -f /var/run/httpd2.1/ssl_scache ] && rm -f /var/run/httpd2.1/*ssl_scache*
-		log_begin_msg "Starting web server (httpd2.1)..."
+		[ -f /var/run/apache2/ssl_scache ] && rm -f /var/run/apache2/*ssl_scache*
+		log_begin_msg "Starting web server (apache2)..."
 		if $APACHE2CTL start; then
                         log_end_msg 0
                 else
@@ -101,7 +101,7 @@ case $1 in
                 fi
 	;;
 	stop)
-		log_begin_msg "Stopping web server (httpd2.1)..."
+		log_begin_msg "Stopping web server (apache2)..."
 		if apache_stop; then
                         log_end_msg 0
                 else
@@ -117,7 +117,7 @@ case $1 in
                 fi
 	;;
 	restart | force-reload)
-		log_begin_msg "Forcing reload of web server  (httpd2.1)..."
+		log_begin_msg "Forcing reload of web server  (apache2)..."
 		if ! apache_stop; then
                         log_end_msg 1
                 fi
@@ -129,6 +129,6 @@ case $1 in
                 fi
 	;;
 	*)
-		log_success_msg "Usage: /etc/init.d/httpd2.1 start|stop|restart|reload|force-reload"
+		log_success_msg "Usage: /etc/init.d/apache2 start|stop|restart|reload|force-reload"
 	;;
 esac

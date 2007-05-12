1858ddf40b19f672e16d6e0abd52eaf83fb32646
Peter Samuelson <peter@p12n.org>
2007-05-12 20:59:41 +0000

    Bug #420101: move the 'chown www-data /var/lock/apache2' from apache2.2-common.postinst to apache2.2-common.init.d, as /var/lock may not persist across reboots.
    Also cut out a couple of existence checks, as neither mkdir -p nor
    install -d complain if their target already exists.
    
    
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-apache/trunk/apache2@318 01b336ce-410b-0410-9a02-a0e7f243c266

--- debian/apache2.2-common.init.d
+++ debian/apache2.2-common.init.d
@@ -97,10 +97,10 @@ apache_stop() {
 case $1 in
 	start)
 		[ -f /etc/apache2/httpd.conf ] || touch /etc/apache2/httpd.conf
-		[ -d /var/run/apache2 ] || mkdir -p /var/run/apache2
-		[ -d /var/lock/apache2 ] || mkdir -p /var/lock/apache2
+		mkdir -p /var/run/apache2
+		install -d -o www-data /var/lock/apache2
 		#ssl_scache shouldn't be here if we're just starting up.
-		[ -f /var/run/apache2/ssl_scache ] && rm -f /var/run/apache2/*ssl_scache*
+		rm -f /var/run/apache2/*ssl_scache*
 		log_begin_msg "Starting web server (apache2)..."
 		if $APACHE2CTL start; then
                         log_end_msg 0
